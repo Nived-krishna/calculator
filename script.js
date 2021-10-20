fab98c1ec661a41107341e71cc2c7fd570a7a495
Nived Krishna <nived050@gmail.com>
2021-10-20 10:01:26 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,22 @@
+// function displayValue(num)
+// {
+//     let res=document.querySelector("#result");
+//     res.value+=num
+// }
+
+var displayValue=(num)=>result.value+=num
+var clearBox=()=>result.value=" "
+var evaluateExpression=()=>result.value=eval(result.value)
+
+
+// function clearBox()
+// {
+//     result.value=" "
+// }
+
+// function evaluateExpression()
+// {
+//     let expr=result.value;//(1-2)
+//     let out=eval(expr)
+//     result.value=out;
+// }
